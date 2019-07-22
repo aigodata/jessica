40b651dff6b7b566838b42008c2558eef1dca13c
xueyuan1018@126.com <xueyuan1018@126.com>
2019-07-22 15:24:29 +0800

    .

--- web/layout/header.vue
+++ web/layout/header.vue
@@ -1,6 +1,19 @@
 <template>
 	<div class="ai-layout-header">
-		header
+		<div class="logo">
+			<img src="./../assets/images/favicon.png">
+			<span>高校大数据平台</span>
+		</div>
+		<ul class="navbar-nav">
+			<li>分析</li>
+			<li>指标</li>
+			<li class="active">数据源</li>
+			<li>设置</li>
+		</ul>
+		<ul class="quick-menu">
+			<li>通知</li>
+			<li>个人信息</li>
+		</ul>
 	</div>
 </template>
 
